e3229db1a449fee0eab9bd7ddca3d6f0b4234b16
mhsyl <mhsyl@192.168.123.162>
2020-09-28 11:56:45 +0900

    calendar printout

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -2,7 +2,13 @@ package honux.calendar;
 
 public class Calendar {
 	public static void main(String[] arg) {
-		System.out.println("This is my calendar project");
+		print();
 	}
-
+	public static void print() {
+		System.out.println(" 월  화   수   목   금   토   일");
+		System.out.println(" 1  2  3  4  5  6  7");
+		System.out.println(" 8  9 10 11 12 13 14");
+		System.out.println("15 16 17 18 19 20 21");
+		System.out.println("22 23 24 25 26 27 28");
+	}
 }
